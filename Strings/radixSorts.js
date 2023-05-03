3091a7b227c50ce6927d2fe4172445074b3559de
JuniFruit@github.com <idiot228322@mail.ru>
2023-05-03 15:49:01 +0300

    lsd sort

--- Strings/radixSorts.js
+++ Strings/radixSorts.js
@@ -17,3 +17,64 @@ export const keyIndexedSort = input => {
   input = aux;
   return input;
 };
+
+export const lsdSort = (strArr, strLen) => {
+  const R = 258;
+  const W = strArr.length;
+  const aux = [];
+
+  for (let i = strLen - 1; i >= 0; i--) {
+    const count = Array(R + 1).fill(0);
+
+    for (let j = 0; j < W; j++) {
+      count[strArr[j].charCodeAt(i) + 1]++;
+    }
+    for (let j = 0; j < R; j++) {
+      count[j + 1] += count[j];
+    }
+    for (let j = 0; j < W; j++) {
+      aux[count[strArr[j].charCodeAt(i)]++] = strArr[j];
+    }
+    for (let j = 0; j < W; j++) {
+      strArr[j] = aux[j];
+    }
+  }
+  return strArr;
+};
+
+export const nonFixedLsd = input => {
+  const N = input.length;
+
+  let W = -Infinity;
+  for (let i = 0; i < N; i++) {
+    input[i] = input[i].toString();
+    W = Math.max(W, input[i].length);
+  }
+
+  const findCode = (arr, ind, key) => {
+    return isNaN(arr[ind].charCodeAt(key)) ? -1 : arr[ind].charCodeAt(key);
+  };
+
+  const R = 258;
+
+  const aux = [];
+  for (let d = W - 1; d >= 0; d--) {
+    const count = Array(R + 1).fill(0);
+
+    for (let i = 0; i < N; i++) {
+      count[findCode(input, i, d) + 2]++;
+    }
+
+    for (let i = 0; i < R; i++) {
+      count[i + 1] += count[i];
+    }
+
+    for (let i = 0; i < N; i++) {
+      aux[count[findCode(input, i, d) + 1]++] = input[i];
+    }
+    for (let i = 0; i < N; i++) {
+      input[i] = aux[i];
+    }
+  }
+  return input;
+};
--- Strings/radixSorts.test.js
+++ Strings/radixSorts.test.js
@@ -1,8 +1,46 @@
-import { keyIndexedSort } from "./radixSorts";
+import { keyIndexedSort, lsdSort, nonFixedLsd } from "./radixSorts";
 
 describe("Radix sorts", () => {
   test("keyIndexedSort should return sorted arr by key ", () => {
     const input = [1, 1, 5, 5, 2, 2, 2, 4, 4];
     expect(keyIndexedSort(input)).toEqual([1, 1, 2, 2, 2, 4, 4, 5, 5]);
+  });
+
+  test("LSD sort should return sorted strings in asc order ", () => {
+    const input = ["she", "has", "car", "low", "end", "run", "buy", "and"];
+    expect(lsdSort(input, 3)).toEqual(["and", "buy", "car", "end", "has", "low", "run", "she"]);
+  });
+  test("LSD float sort should return sorted strings in asc order ", () => {
+    const input = [
+      "she",
+      "has",
+      "car",
+      "low",
+      "end",
+      "run",
+      "buy",
+      "and",
+      "float",
+      "fire",
+      "bound",
+      "andy",
+    ];
+    expect(nonFixedLsd(input)).toEqual([
+      "and",
+      "andy",
+      "bound",
+      "buy",
+      "car",
+      "end",
+      "fire",
+      "float",
+      "has",
+      "low",
+      "run",
+      "she",
+    ]);
+
+    const input2 = ["23", "345", "5467", "12", "2345", "9852"];
+    expect(nonFixedLsd(input2)).toEqual(["12", "23", "2345", "345", "5467", "9852"]);
   });
 });
